a2b5cfcb64f5e5c3ecc4ac97ebf9f089d4c81048
renelps <renanlps616@gmail.com>
2025-12-16 16:13:11 -0300

    feat(home): implement product fetching and ProductList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
+import { Product } from "@/types/products";
+import api from "@/utils/axios-instance";
+import { ProductList } from "./components/product-list";
 
 
-export default function Home() {
+const findAllProducts = async (): Promise<Product[]> => { 
+
+  try {
+    return (await api.get('product')).data 
+  }catch(err) {
+    console.log('error searching for products', err);
+    return [];
+  }
+
+}
+
+export default async function Home() {
+  const products: Product[] = await findAllProducts()
+
+  if(!products) {
+    return <div>Produtos não encontrados...</div>
+  }
+
   return (
-    <div className="w-full h-screen">
-      
+    <div className="w-full h-screen p-10">
+      <ProductList data={products}/>
     </div>
   );
 }
