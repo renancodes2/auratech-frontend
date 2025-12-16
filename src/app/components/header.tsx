b44cad0d71cbb69dab57f5e2f3d94de8359c79ad
renelps <renanlps616@gmail.com>
2025-12-16 16:12:09 -0300

    feat(ui): update header with cart icon and mobile menu navigation

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,12 +3,11 @@
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { FaBars } from 'react-icons/fa'
+import { FaBars, FaShoppingBag, FaUser } from 'react-icons/fa'
 import { useState } from "react"
 
 export function Header() {
@@ -24,22 +23,35 @@ export function Header() {
           <h2 className="font-bold text-2xl">AURA <span className="text-red-500">TECH</span></h2>
         </div>
         <div>
-          <h2> 
-            <SheetTrigger className="cursor-pointer md:hidden">
-              <FaBars size={24} color="#fff" />
-            </SheetTrigger>
-          </h2>
+          <SheetTrigger className="cursor-pointer md:hidden">
+            <FaBars size={24} color="#fff" />
+          </SheetTrigger>
+          <div className="hidden md:flex">
+            <div className="relative">
+              <FaShoppingBag size={24} color="#ddd" />
+
+              <p 
+                className="w-4 h-4 bg bg-[linear-gradient(90deg,#7c5cff,#4dd0e1)] text-[#061226] shadow-[0_6px_18px_rgba(124,92,255,0.18)] rounded-full flex items-center justify-center text-[10px] absolute left-3 top-4"
+              >
+                1
+              </p>
+            </div>
+          </div>
         </div>
       </div>
      
-      <SheetContent className="z-99">
+      <SheetContent className="bg-black">
         <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your account
-            and remove your data from our servers.
-          </SheetDescription>
+          <SheetTitle>Menu</SheetTitle>
         </SheetHeader>
+          <div className="flex items-center pl-3 gap-2">
+            <FaUser size={24} color="#ddd" />
+            <p>Perfil</p>
+          </div>
+          <div className="flex items-center pl-3 gap-2">
+            <FaShoppingBag size={24} color="#ddd" />
+            <p>Carrinho</p>
+          </div>
       </SheetContent>
     </Sheet>
     </>
