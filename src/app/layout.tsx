c8e914d0c9823af61b945404eb7ec2f9f76e9ab3
renelps <renanlps616@gmail.com>
2025-12-19 08:19:59 -0300

    git commit -m "feat: add global sonner toaster and remove sidebar provider"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SidebarProvider } from "@/components/ui/sidebar";
 import { Header } from "./components/header";
+import { Toaster } from "sonner"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster 
+          duration={3000}
+          position="top-right"
+        />
         <Header />
         {children}
       </body>
