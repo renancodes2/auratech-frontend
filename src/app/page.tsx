d935eadfdab72c996aa6786cdb33416c6bf69a20
renelps <renanlps616@gmail.com>
2025-12-22 12:23:48 -0300

    fix(home): set revalidate to 0 to bypass SSG cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,15 @@ import { Product } from "@/types/products";
 import api from "@/utils/axios-instance";
 import { ProductList } from "./components/product-list";
 
+export const revalidate = 120;
 
 const findAllProducts = async (): Promise<Product[]> => { 
-
   try {
     return (await api.get('product')).data 
   }catch(err) {
     console.log('error searching for products', err);
     return [];
   }
-
 }
 
 export default async function Home() {
